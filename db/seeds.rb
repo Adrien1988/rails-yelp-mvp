c72c49a465345036b09c28f53f9c83876fd3323e
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2021-05-12 16:21:19 +0200

    complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bristol = Restaurant.create!(name: "Epicure",address: "7 rue de la paie, Paris", phone_number:"0145697521", category: "french")
+
+jimi = Restaurant.create!(name: "Jimi",address: "31 rue de Gaule, Paris", phone_number:"0145697562", category: "chinese")
+
+meurice = Restaurant.create!(name: "Meurice",address: "les Tuilleries, Paris", phone_number:"0145697587", category: "French")
+
+frite = Restaurant.create!(name: "La frite",address: "Nation, Paris", phone_number:"0145697555", category: "belgian")
+
